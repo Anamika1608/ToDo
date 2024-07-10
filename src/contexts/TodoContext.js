e6d71414b39a1e3201b2acb9a54596cfc7809721
Anamika1608 <anamikaagg18@gmail.com>
2024-07-10 23:17:16 +0530

    first commit

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext,useContext } from "react";
+import { v4 as uuidv4 } from 'uuid';
+export const TodoContext = createContext({
+    todos : [
+        {
+            id: uuidv4(),
+            todo : "Learn react",
+            completed: false,
+        }
+    ],
+    addTodo : (todo)=>{},
+    updateTodo : (todo,id)=>{},
+    deleteTodo : (id)=>{},
+    toggleComplete : (id)=>{},
+})
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider;
